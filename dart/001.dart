c1bb5561adfef0978a9d4da9818ce1e8e055e2a2
yongboo <shanyongbo2008@gmail.com>
2019-05-04 23:33:57 +0800

    update

---
+++ dart/001.dart
@@ -0,0 +1,20 @@
+// 类
+class Developer {
+  // 实例方法
+  say() {
+    // print是全局打印函数，类似于JavaScript的console.log和Java的System.out.print
+    print('Hello World');
+  }
+}
+
+// 顶层函数
+sayHello() {
+  new Developer().say();
+}
+
+// 执行入口
+void main() {
+  var a;
+  print(a);
+  sayHello();
+}
